a7b5d336d53707f46da990d0c07eead9d55cdbc7
Charllys Fernandes <charllys.daiany@gmail.com>
2023-02-27 22:23:55 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,184 +1,110 @@
-// selecionando o botão "Salvar" dentro do modal
-const btnSalvar = document.querySelector("#btn-salvar");
-const tableBody = document.querySelector("#table-body");
+let novaTabela = (i, nome, email, telefone) => `
+<th scope="row">${i + 1}</th>
+      <td>${nome}</td>
+      <td>${email}</td>
+      <td>${telefone}</td>
+      <td>
+        <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal"
+          onclick="preencheFormulario(${i})">Alterar</button>
+      </td>
+      <td>
+        <button type="button" class="btn btn-danger btn-sm" onclick="excluirCliente(${i})">Excluir</button>
+      </td>
+`
 
-// verificando se há clientes salvos no localStorage ao carregar a página
-if (localStorage.getItem("clientes")) {
-  renderizaClientesNaTabela();
-}
+const btnModalSalvar = document.getElementById("btnModalSalvar");
 
-// adicionando um evento de clique no botão "Salvar"
+btnModalSalvar.addEventListener('click', function () {
+  salvarCliente();
+})
 
-function validarCampos() {
-  const nome = document.querySelector("#input-nome").value;
-  const email = document.querySelector("#input-email").value;
-  const celular = formatPhoneNumber(document.querySelector("#input-celular").value);
-  const cidade = document.querySelector("#input-cidade").value;
+const clientes = JSON.parse(localStorage.getItem("clientes")) || [];
+atualizarTabela(clientes);
 
-  if (!nome || !email || !celular || !cidade) {
-    alert("Por favor, preencha todos os campos.");
-    return false;
-  }
-  btnSalvar.setAttribute("data-bs-dismiss", "modal");
-  return true;
-}
+function salvarCliente() {
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const telefone = formatarTelefone(document.getElementById("telefone").value);
 
-function salvarDados() {
-  if (!validarCampos()) {
+  // Verifica se os campos estão vazios
+  if (nome === "" || email === "" || telefone === "") {
+    alert("Preencha todos os campos antes de salvar o cliente.");
     return;
   }
 
-  const nome = document.querySelector("#input-nome").value;
-  const email = document.querySelector("#input-email").value;
-  const celular = formatPhoneNumber(document.querySelector("#input-celular").value);
-  const cidade = document.querySelector("#input-cidade").value;
-
-  const formData = {
+  const novoCliente = {
     nome,
     email,
-    celular,
-    cidade,
+    telefone,
   };
 
-  const clientes = JSON.parse(localStorage.getItem("clientes")) || [];
-
-  clientes.push(formData);
-
+  clientes.push(novoCliente);
   localStorage.setItem("clientes", JSON.stringify(clientes));
+  limparFormulario();
+  fecharModal();
+  atualizarTabela(clientes);
 
-  const modal = document.querySelector("#myModal");
-  const modalBootstrap = bootstrap.Modal.getInstance(modal);
-  modalBootstrap._backdrop.remove();
-  modalBootstrap.hide();
+}
 
-  const form = document.querySelector("#myForm");
+function verficarCamposVazio() {
+  if (nome === "" || email === "" || telefone === "") {
+    alert("Preencha todos os campos antes de salvar o cliente.");
+    return;
+  }
+}
+
+function fecharModal() {
+  let modalElement = document.getElementById('exampleModal');
+  let modal = bootstrap.Modal.getInstance(modalElement);
+  modal.hide();
+}
+
+function limparFormulario() {
+  const form = document.querySelector("#modal-form");
   form.reset();
-  renderizaClientesNaTabela();
 }
 
-function formatPhoneNumber(phoneNumberString) {
-  // Remove tudo que não é número
-  const cleaned = phoneNumberString.replace(/\D/g, "");
+function atualizarTabela(clientes) {
+  const tbody = document.getElementById('tbody');
+  tbody.innerHTML = '';
 
-  // Verifica se o número tem 10 ou 11 dígitos
-  const match = cleaned.match(/^(\d{2})(\d{4,5})(\d{4})$/);
-  if (match) {
-    // Formata o número com 10 dígitos
-    return `(${match[1]}) ${match[2]}-${match[3]}`;
-  }
+  for (let i = 0; i < clientes.length; i++) {
+    const cliente = clientes[i];
 
-  const match9 = cleaned.match(/^(\d{2})(\d{5})(\d{4})$/);
-  if (match9) {
-    // Formata o número com 9 dígitos
-    return `(${match9[1]}) ${match9[2]}-${match9[3]}`;
-  }
+    const tr = document.createElement('tr');
+    let nome = cliente.nome
+    let email = cliente.email
+    let telefone = cliente.telefone
+    tr.innerHTML = novaTabela(i, nome, email, telefone);
 
-  // Se o número não tem 10 ou 11 dígitos, retorna o número original
-  return phoneNumberString;
-}
-
-function renderizaClientesNaTabela() {
-  if (localStorage.getItem("clientes")) {
-    // recuperando os dados salvos do localStorage
-    const clientesArray = JSON.parse(localStorage.getItem("clientes"));
-
-    // limpando o conteúdo da tabela
-    tableBody.innerHTML = "";
-
-    // percorrendo o array de dados e adicionando as linhas na tabela
-    clientesArray.forEach((cliente) => {
-      // criando a linha da tabela
-      const row = document.createElement("tr");
-
-      // criando as células da linha com os dados do cliente
-      const nomeCell = document.createElement("td");
-      nomeCell.innerText = cliente.nome;
-      row.appendChild(nomeCell);
-
-      const emailCell = document.createElement("td");
-      emailCell.innerText = cliente.email;
-      row.appendChild(emailCell);
-
-      const celularCell = document.createElement("td");
-      celularCell.innerText = cliente.celular;
-      row.appendChild(celularCell);
-
-      const cidadeCell = document.createElement("td");
-      cidadeCell.innerText = cliente.cidade;
-      row.appendChild(cidadeCell);
-
-      // criando a célula para os botões de ação
-      const acaoCell = document.createElement("td");
-
-
-      // adicionando botão "Alterar"
-      const btnAlterar = document.createElement("button");
-      btnAlterar.innerText = "Alterar";
-      btnAlterar.classList.add("btn", "btn-success", "me-2");
-      btnAlterar.addEventListener("click", () => {
-        // adicionar a função para alterar o cliente aqui
-      });
-      acaoCell.appendChild(btnAlterar);
-
-      // adicionando botão "Excluir"
-      const btnExcluir = document.createElement("button");
-      btnExcluir.innerText = "Excluir";
-      btnExcluir.classList.add("btn", "btn-danger");
-      btnExcluir.addEventListener("click", () => {
-        excluirCliente(btnExcluir);
-      });
-      acaoCell.appendChild(btnExcluir);
-
-      row.appendChild(acaoCell);
-
-      // adicionando a linha na tabela
-      tableBody.appendChild(row);
-    });
+    tbody.appendChild(tr);
   }
 }
 
-function excluirCliente(btnExcluir) {
-  const rowIndex = btnExcluir.closest('tr').rowIndex - 1; // obtém o índice da linha da tabela
-  const clientesArray = JSON.parse(localStorage.getItem("clientes")); // obtém o array de clientes salvos no localStorage
-  clientesArray.splice(rowIndex, 1); // remove o cliente correspondente do array
-  localStorage.setItem("clientes", JSON.stringify(clientesArray)); // salva o array atualizado no localStorage
-  renderizaClientesNaTabela(); // atualiza a tabela exibida na página
+function excluirCliente(i) {
+  clientes.splice(i, 1);
+  localStorage.setItem("clientes", JSON.stringify(clientes));
+  atualizarTabela(clientes);
 }
 
-
-
-
-btnSalvar.addEventListener("click", salvarDados);
-document.getElementById("input-celular").addEventListener('input', function (event) {
-  var phoneNumber = event.target.value;
-  event.target.value = formatPhoneNumber(phoneNumber);
-});
-
-document.addEventListener("click", (event) => {
-  if (event.target.classList.contains("btn-alterar")) {
-    // Obtem a linha da tabela onde o botão "Alterar" foi clicado
-    const linha = event.target.parentNode.parentNode;
-
-    // Obtem os dados do cliente selecionado na tabela
-    const nome = linha.querySelector("td:nth-child(1)").innerText;
-    const email = linha.querySelector("td:nth-child(2)").innerText;
-    const celular = linha.querySelector("td:nth-child(3)").innerText;
-    const cidade = linha.querySelector("td:nth-child(4)").innerText;
-
-    // Define os valores dos inputs no modal
-    const inputNome = document.getElementById("input-nome");
-    const inputEmail = document.getElementById("input-email");
-    const inputCelular = document.getElementById("input-celular");
-    const inputCidade = document.getElementById("input-cidade");
-
-    inputNome.value = nome;
-    inputEmail.value = email;
-    inputCelular.value = celular;
-    inputCidade.value = cidade;
-
-    // Adiciona um atributo data-cliente-id com o id do cliente para ser usado na hora de salvar as alterações
-    const clienteId = linha.getAttribute("data-cliente-id");
-    const btnSalvar = document.getElementById("btn-salvar");
-    btnSalvar.setAttribute("data-cliente-id", clienteId);
+function formatarTelefone(telefone) {
+  const telefoneNumerico = telefone.replace(/\D/g, "");
+  if (telefoneNumerico.length < 10 || telefoneNumerico.length > 11) {
+    return telefone;
   }
-});
+  const ddd = telefoneNumerico.slice(0, 2);
+  let numero = telefoneNumerico.slice(2);
+
+  if (numero.length === 8) {
+    numero = `${numero.slice(0, 4)}-${numero.slice(4)}`;
+  } else {
+    numero = `${numero.slice(0, 5)}-${numero.slice(5)}`;
+    if (ddd) {
+      numero = `(${ddd}) ${numero}`;
+    }
+  }
+  return numero;
+}
+
+// TO-DO corrija input do telefone.
+
